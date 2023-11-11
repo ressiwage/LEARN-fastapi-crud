89e00bdd52c1c60b598c13178d51c81f0f8f3a7b
1 <ressiwage@ya.ru>
2023-11-11 17:20:05 +0500

    docker

--- setup.sh
+++ setup.sh
@@ -14,3 +14,6 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+#compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
